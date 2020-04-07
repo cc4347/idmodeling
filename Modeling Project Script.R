4953a9fae63fcd257e9e5245fad14b4864bef236
Cassie Coulter <alexandercoulter@alexandersMini2.hsd1.pa.comcast.net>
2020-04-07 16:04:09 -0400

    Created a script file for models

---
+++ Modeling Project Script.R
@@ -0,0 +1,3 @@
+################# ID Modeling Group Project #############################
+###### SEIR/SIRS Models for Cholera Outbreak in Haiti (2010) With a an Intervention
+###### (Vaccination) and a Past Outbreak
