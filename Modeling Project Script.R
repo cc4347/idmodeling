b0cec87f16c02d9c4eff56c40d9e36d75ca2f53b
atseng1 <at3346@cumc.columbia.edu>
2020-04-08 13:05:51 -0400

    deleted R script file

--- Modeling Project Script.R
+++
@@ -1,3 +0,0 @@
-################# ID Modeling Group Project #############################
-###### SEIR/SIRS Models for Cholera Outbreak in Haiti (2010) With a an Intervention
-###### (Vaccination) and a Past Outbreak
